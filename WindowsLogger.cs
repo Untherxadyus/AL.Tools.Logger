d702ef2655d364726e88749043204401706fe864
Aaron Vazquez <Aaron.Vazquez@live.com>
2019-11-18 14:51:11 -0500

    Added method to accept an exception and log an error entry.

--- WindowsLogger.cs
+++ WindowsLogger.cs
@@ -14,9 +14,18 @@ namespace AL.Tools.Logger
 
         static WindowsLogger()
         {
+            //Get Configuration Values
             var Source = Settings.AppSettings("AL-LogSource");
             var Log = Settings.AppSettings("AL-LogName");
 
+            //Set default values in case of missing configuration
+            if(Source.IsEmpty())
+                Source = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+
+            if (Log.IsEmpty())
+                Log = "Application";
+            
+            //Create Source if it doesn't exist
             if (EventLog.SourceExists(Source) == false)
                 EventLog.CreateEventSource(Source, Log);
 
@@ -32,6 +41,11 @@ namespace AL.Tools.Logger
         public static void AddEntry(string Message, EventLogEntryType LogType = EventLogEntryType.Information)
         {
             ApplicationLog.WriteEntry(Message, LogType);
+        }
+
+        public static void AddEntry(Exception ex)
+        {
+            ApplicationLog.WriteEntry(ex.Message, EventLogEntryType.Error);
         }
     }
 }
